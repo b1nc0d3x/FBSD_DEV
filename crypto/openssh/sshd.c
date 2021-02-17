markjdb/freebsd

openssh: refer to OpenSSL not SSLeay
This change was made upstream between 7.9p1 and 8.0p1.  We've made local
changes in the same places for handling the version_addendum; apply the
SSLeay_version to OpenSSL_version change in advance of importing 8.0p1.

Obtained from:	OpenSSH-portable a65784c9f9c5
Sponsored by:	The FreeBSD Foundation

crypto/openssh/sshd.c
@@ -1749,7 +1749,7 @@ main(int ac, char **av)
 
 	debug("sshd version %s, %s", SSH_VERSION,
 #ifdef WITH_OPENSSL
-	    SSLeay_version(SSLEAY_VERSION)
+	    OpenSSL_version(SSLEAY_VERSION)
 #else
 	    "without OpenSSL"
 #endif

crypto/openssh/version.h
@@ -9,7 +9,7 @@
 #define SSH_VERSION_FREEBSD	"FreeBSD-20200214"
 
 #ifdef WITH_OPENSSL
-#define OPENSSL_VERSION_STRING	SSLeay_version(SSLEAY_VERSION)
+#define OPENSSL_VERSION_STRING	OpenSSL_version(SSLEAY_VERSION)
 #else
 #define OPENSSL_VERSION_STRING	"without OpenSSL"
 #endif

b1nc0d3x/FBSD_DEV

(cherry picked from commit 6471c6bd75af57acde7dc39f5202d9a7adf4130a)
openssh: refer to OpenSSL not SSLeay, part 2
(cherry picked from commit ea64ebd08c80e4c0a7b8ed207caba45e9374908e)

crypto/openssh/sshd.c
@@ -1749,7 +1749,7 @@ main(int ac, char **av)
 
 	debug("sshd version %s, %s", SSH_VERSION,
 #ifdef WITH_OPENSSL
-	    SSLeay_version(SSLEAY_VERSION)
+	    OpenSSL_version(OPENSSL_VERSION)
 #else
 	    "without OpenSSL"
 #endif

crypto/openssh/version.h
@@ -9,7 +9,7 @@
 #define SSH_VERSION_FREEBSD	"FreeBSD-20200214"
 
 #ifdef WITH_OPENSSL
-#define OPENSSL_VERSION_STRING	SSLeay_version(SSLEAY_VERSION)
+#define OPENSSL_VERSION_STRING	OpenSSL_version(OPENSSL_VERSION)
 #else
 #define OPENSSL_VERSION_STRING	"without OpenSSL"
 #endif