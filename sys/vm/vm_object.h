freebsd/freebsd-src

vm: add another pager private flag
Move OBJ_SHADOWLIST around to let pager flags be next to each other.

Reviewed by:	kib, markj
Differential Revision:	https://reviews.freebsd.org/D30258

sys/vm/vm_object.h
@@ -201,13 +201,14 @@ struct vm_object {
 #define	OBJ_UMTXDEAD	0x0020		/* umtx pshared was terminated */
 #define	OBJ_SIZEVNLOCK	0x0040		/* lock vnode to check obj size */
 #define	OBJ_PG_DTOR	0x0080		/* dont reset object, leave that for dtor */
+#define	OBJ_SHADOWLIST	0x0100		/* Object is on the shadow list. */
 #define	OBJ_SWAP	0x0200		/* object swaps */
 #define	OBJ_SPLIT	0x0400		/* object is being split */
 #define	OBJ_COLLAPSING	0x0800		/* Parent of collapse. */
 #define	OBJ_COLORED	0x1000		/* pg_color is defined */
 #define	OBJ_ONEMAPPING	0x2000		/* One USE (a single, non-forked) mapping flag */
-#define	OBJ_SHADOWLIST	0x4000		/* Object is on the shadow list. */
-#define	OBJ_PAGERPRIV	0x8000		/* Pager private */
+#define	OBJ_PAGERPRIV1	0x4000		/* Pager private */
+#define	OBJ_PAGERPRIV2	0x8000		/* Pager private */
 
 /*
  * Helpers to perform conversion between vm_object page indexes and offsets.

b1nc0d3x/FBSD_DEV

Contrary to what was done in main, skip the following in order to not
disrupt KBI:
(cherry picked from commit 128e25842ef510b39815d784e1d2af0d4afd584f)

sys/vm/vm_object.h
@@ -201,13 +201,14 @@ struct vm_object {
 #define	OBJ_UMTXDEAD	0x0020		/* umtx pshared was terminated */
 #define	OBJ_SIZEVNLOCK	0x0040		/* lock vnode to check obj size */
 #define	OBJ_PG_DTOR	0x0080		/* dont reset object, leave that for dtor */
+#define	OBJ_PAGERPRIV2	0x0100		/* Pager private */
 #define	OBJ_SWAP	0x0200		/* object swaps */
 #define	OBJ_SPLIT	0x0400		/* object is being split */
 #define	OBJ_COLLAPSING	0x0800		/* Parent of collapse. */
 #define	OBJ_COLORED	0x1000		/* pg_color is defined */
 #define	OBJ_ONEMAPPING	0x2000		/* One USE (a single, non-forked) mapping flag */
-#define	OBJ_SHADOWLIST	0x4000		/* Object is on the shadow list. */
-#define	OBJ_PAGERPRIV	0x8000		/* Pager private */
+#define	OBJ_PAGERPRIV1	0x4000		/* Pager private */
+#define	OBJ_SHADOWLIST	0x8000		/* Object is on the shadow list. */
 
 /*
  * Helpers to perform conversion between vm_object page indexes and offsets.