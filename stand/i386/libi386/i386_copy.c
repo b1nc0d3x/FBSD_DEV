markjdb/freebsd

cstyle cleanup for i386_copy.c
No functional changes are intended.

stand/i386/libi386/i386_copy.c
@@ -40,25 +40,25 @@ __FBSDID("$FreeBSD$");
 ssize_t
 i386_copyin(const void *src, vm_offset_t dest, const size_t len)
 {
-    if (dest + len >= memtop) {
-	errno = EFBIG;
-	return(-1);
-    }
+	if (dest + len >= memtop) {
+		errno = EFBIG;
+		return (-1);
+	}
 
-    bcopy(src, PTOV(dest), len);
-    return(len);
+	bcopy(src, PTOV(dest), len);
+	return (len);
 }
 
 ssize_t
 i386_copyout(const vm_offset_t src, void *dest, const size_t len)
 {
-    if (src + len >= memtop) {
-	errno = EFBIG;
-	return(-1);
-    }
-    
-    bcopy(PTOV(src), dest, len);
-    return(len);
+	if (src + len >= memtop) {
+		errno = EFBIG;
+		return (-1);
+	}
+
+	bcopy(PTOV(src), dest, len);
+	return (len);
 }
 
 
@@ -66,10 +66,10 @@ ssize_t
 i386_readin(const int fd, vm_offset_t dest, const size_t len)
 {
 
-    if (dest + len >= memtop_copyin) {
-	errno = EFBIG;
-	return(-1);
-    }
+	if (dest + len >= memtop_copyin) {
+		errno = EFBIG;
+		return (-1);
+	}
 
-    return (read(fd, PTOV(dest), len));
+	return (read(fd, PTOV(dest), len));
 }

b1nc0d3x/FBSD_DEV

(cherry picked from commit 1113c41fef7b1b0ff02d816908f553f4699d9bd1)

stand/i386/libi386/i386_copy.c
@@ -40,25 +40,25 @@ __FBSDID("$FreeBSD$");
 ssize_t
 i386_copyin(const void *src, vm_offset_t dest, const size_t len)
 {
-    if (dest + len >= memtop) {
-	errno = EFBIG;
-	return(-1);
-    }
+	if (dest + len >= memtop) {
+		errno = EFBIG;
+		return (-1);
+	}
 
-    bcopy(src, PTOV(dest), len);
-    return(len);
+	bcopy(src, PTOV(dest), len);
+	return (len);
 }
 
 ssize_t
 i386_copyout(const vm_offset_t src, void *dest, const size_t len)
 {
-    if (src + len >= memtop) {
-	errno = EFBIG;
-	return(-1);
-    }
-    
-    bcopy(PTOV(src), dest, len);
-    return(len);
+	if (src + len >= memtop) {
+		errno = EFBIG;
+		return (-1);
+	}
+
+	bcopy(PTOV(src), dest, len);
+	return (len);
 }
 
 
@@ -66,10 +66,10 @@ ssize_t
 i386_readin(readin_handle_t fd, vm_offset_t dest, const size_t len)
 {
 
-    if (dest + len >= memtop_copyin) {
-	errno = EFBIG;
-	return(-1);
-    }
+	if (dest + len >= memtop_copyin) {
+		errno = EFBIG;
+		return (-1);
+	}
 
-    return (VECTX_READ(fd, PTOV(dest), len));
+	return (VECTX_READ(fd, PTOV(dest), len));
 }