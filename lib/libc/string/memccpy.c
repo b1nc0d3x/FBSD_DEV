clausecker/freebsd-src

lib/libc/string: document restrict qualification of memccpy() arguments
POSIX.1-2004 and the upcoming C23 agree that memccpy()'s arguments
are restrict qualified and must not overlap.  In 2002, restrict
qualifiers were added to <string.h>'s declaration of the function.
Make things official and document that the arguments must not
overlap.

See also:	32484af1c42ce18acd88c7e8b7c54f5e312029cc

lib/libc/string/memccpy.c
@@ -36,7 +36,7 @@ static char sccsid[] = "@(#)memccpy.c	8.1 (Berkeley) 6/4/93";
 #include <string.h>
 
 void *
-memccpy(void *t, const void *f, int c, size_t n)
+memccpy(void * __restrict t, const void * __restrict f, int c, size_t n)
 {
 
 	if (n) {

b1nc0d3x/FBSD_DEV

See also:	61b60edfd3fff20f884419f8097870c7045315c9
Approved by:	kib
MFC after:	1 month
MFC to:		stable/14
(cherry picked from commit e0d4f419ac41aa91b862f3ceadc32a86abf08572)

lib/libc/string/memccpy.c
@@ -35,7 +35,7 @@ static char sccsid[] = "@(#)memccpy.c	8.1 (Berkeley) 6/4/93";
 #include <string.h>
 
 void *
-memccpy(void *t, const void *f, int c, size_t n)
+memccpy(void * restrict t, const void * restrict f, int c, size_t n)
 {
 
 	if (n) {