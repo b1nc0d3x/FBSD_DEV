freebsd/freebsd-src

efirt: mark dynamic region executing the RT code with the TDP_EFIRT flag
Reviewed by:	markj
Sponsored by:	The FreeBSD Foundation
MFC after:	1 week
Differential revision:	https://reviews.freebsd.org/D47694

sys/sys/proc.h
@@ -560,7 +560,7 @@ enum {
 #define	TDP_RESETSPUR	0x04000000 /* Reset spurious page fault history. */
 #define	TDP_NERRNO	0x08000000 /* Last errno is already in td_errno */
 #define	TDP_UIOHELD	0x10000000 /* Current uio has pages held in td_ma */
-#define	TDP_UNUSED0	0x20000000 /* UNUSED */
+#define	TDP_EFIRT	0x20000000 /* In firmware (EFI RT) call */
 #define	TDP_EXECVMSPC	0x40000000 /* Execve destroyed old vmspace */
 #define	TDP_SIGFASTPENDING 0x80000000 /* Pending signal due to sigfastblock */
 

b1nc0d3x/FBSD_DEV

For MFC purposes, the TDP_EFIRT flag is moved to td_pflags2.
(cherry picked from commit a03957a706a05bb0c2f627183b19d84f6d96dbb7)

sys/sys/proc.h
@@ -567,6 +567,7 @@ enum {
 #define	TDP2_SBPAGES	0x00000001 /* Owns sbusy on some pages */
 #define	TDP2_COMPAT32RB	0x00000002 /* compat32 ABI for robust lists */
 #define	TDP2_ACCT	0x00000004 /* Doing accounting */
+#define	TDP2_EFIRT	0x20000000 /* In firmware (EFI RT) call */
 
 /*
  * Reasons that the current thread can not be run yet.