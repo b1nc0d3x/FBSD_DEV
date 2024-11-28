freebsd/freebsd-src

amd64 efi rt: handle #BP
PR:	282860
Reviewed by:	markj
Sponsored by:	The FreeBSD Foundation
MFC after:	1 week
Differential revision:	https://reviews.freebsd.org/D47694

sys/amd64/amd64/trap.c
@@ -599,6 +599,18 @@ trap(struct trapframe *frame)
 			 */
 		case T_BPTFLT:
 			/*
+			 * Most likely, EFI RT hitting INT3.  This
+			 * check prevents kdb from handling
+			 * breakpoints set on the BIOS text, if such
+			 * option is ever needed.
+			 */
+			if ((td->td_pflags & TDP_EFIRT) != 0 &&
+			    curpcb->pcb_onfault != NULL) {
+				frame->tf_rip = (long)curpcb->pcb_onfault;
+				return;
+			}
+
+			/*
 			 * If KDB is enabled, let it handle the debugger trap.
 			 * Otherwise, debugger traps "can't happen".
 			 */

b1nc0d3x/FBSD_DEV

(cherry picked from commit e6ec41fa86d88f80bd663e55455a6844619a9b24)

sys/amd64/amd64/trap.c
@@ -601,6 +601,18 @@ trap(struct trapframe *frame)
 			 */
 		case T_BPTFLT:
 			/*
+			 * Most likely, EFI RT hitting INT3.  This
+			 * check prevents kdb from handling
+			 * breakpoints set on the BIOS text, if such
+			 * option is ever needed.
+			 */
+			if ((td->td_pflags2 & TDP2_EFIRT) != 0 &&
+			    curpcb->pcb_onfault != NULL) {
+				frame->tf_rip = (long)curpcb->pcb_onfault;
+				return;
+			}
+
+			/*
 			 * If KDB is enabled, let it handle the debugger trap.
 			 * Otherwise, debugger traps "can't happen".
 			 */