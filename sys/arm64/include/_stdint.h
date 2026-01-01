clausecker/freebsd-src

sys/stdint.h: add C23 _WIDTH macros
Summary:
The platform-dependent macros are added to the various
_stdint.h headers, those that are always the same are
added directly to _stdint.h.

We may want to move the definitions for WCHAR_* and
WINT_* out of the platform header files as those are
always the same.

Test Plan: see following DR

Reviewers: imp, markj, andrew, manu

Subscribers: emaste, jhibbits, #riscv

Differential Revision: https://reviews.freebsd.org/D53830

sys/arm64/include/_stdint.h
@@ -155,6 +155,29 @@
 #define	WINT_MIN	INT32_MIN
 #define	WINT_MAX	INT32_MAX
 
+#if __ISO_C_VISIBLE >= 2023
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.2 Widths of specified-width integer types
+ */
+#define INT_FAST8_WIDTH         INT32_WIDTH
+#define INT_FAST16_WIDTH        INT32_WIDTH
+#define INT_FAST32_WIDTH        INT32_WIDTH
+#define INT_FAST64_WIDTH        INT64_WIDTH
+#define INTPTR_WIDTH            INT32_WIDTH
+#define INTMAX_WIDTH            INT64_WIDTH
+
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.3 Width of other integer types
+ */
+#define PTRDIFF_WIDTH           INT64_WIDTH
+#define SIG_ATOMIC_WIDTH        INT64_WIDTH
+#define SIZE_WIDTH              INT64_WIDTH
+#define WCHAR_WIDTH             INT32_WIDTH
+#define WINT_WIDTH              INT32_WIDTH
+#endif /* __ISO_C_VISIBLE >= 2023 */
+
 #endif /* !defined(__cplusplus) || defined(__STDC_LIMIT_MACROS) */
 
 #endif /* !_MACHINE__STDINT_H_ */

sys/riscv/include/_stdint.h
@@ -151,6 +151,29 @@
 #define	WINT_MIN	INT32_MIN
 #define	WINT_MAX	INT32_MAX
 
+#if __ISO_C_VISIBLE >= 2023
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.2 Widths of specified-width integer types
+ */
+#define INT_FAST8_WIDTH		INT32_WIDTH
+#define INT_FAST16_WIDTH	INT32_WIDTH
+#define INT_FAST32_WIDTH	INT32_WIDTH
+#define INT_FAST64_WIDTH	INT64_WIDTH
+#define INTPTR_WIDTH		INT32_WIDTH
+#define INTMAX_WIDTH		INT64_WIDTH
+
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.3 Width of other integer types
+ */
+#define PTRDIFF_WIDTH		INT64_WIDTH
+#define SIG_ATOMIC_WIDTH	INT64_WIDTH
+#define SIZE_WIDTH		INT64_WIDTH
+#define WCHAR_WIDTH		INT32_WIDTH
+#define WINT_WIDTH		INT32_WIDTH
+#endif /* __ISO_C_VISIBLE >= 2023 */
+
 #endif /* !defined(__cplusplus) || defined(__STDC_LIMIT_MACROS) */
 
 #endif /* !_MACHINE__STDINT_H_ */

b1nc0d3x/FBSD_DEV

Approved by:	markj (mentor)
MFC after:	1 month
Differential Revision:	https://reviews.freebsd.org/D53830
(cherry picked from commit 7998a82d2f99a00086baf64b2a3343ba10f8806d)

sys/arm64/include/_stdint.h
@@ -155,6 +155,29 @@
 #define	WINT_MIN	INT32_MIN
 #define	WINT_MAX	INT32_MAX
 
+#if __ISO_C_VISIBLE >= 2023
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.2 Widths of specified-width integer types
+ */
+#define INT_FAST8_WIDTH         INT32_WIDTH
+#define INT_FAST16_WIDTH        INT32_WIDTH
+#define INT_FAST32_WIDTH        INT32_WIDTH
+#define INT_FAST64_WIDTH        INT64_WIDTH
+#define INTPTR_WIDTH            INT64_WIDTH
+#define INTMAX_WIDTH            INT64_WIDTH
+
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.3 Width of other integer types
+ */
+#define PTRDIFF_WIDTH           INT64_WIDTH
+#define SIG_ATOMIC_WIDTH        INT64_WIDTH
+#define SIZE_WIDTH              INT64_WIDTH
+#define WCHAR_WIDTH             INT32_WIDTH
+#define WINT_WIDTH              INT32_WIDTH
+#endif /* __ISO_C_VISIBLE >= 2023 */
+
 #endif /* !defined(__cplusplus) || defined(__STDC_LIMIT_MACROS) */
 
 #endif /* !_MACHINE__STDINT_H_ */

sys/riscv/include/_stdint.h
@@ -151,6 +151,29 @@
 #define	WINT_MIN	INT32_MIN
 #define	WINT_MAX	INT32_MAX
 
+#if __ISO_C_VISIBLE >= 2023
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.2 Widths of specified-width integer types
+ */
+#define INT_FAST8_WIDTH		INT32_WIDTH
+#define INT_FAST16_WIDTH	INT32_WIDTH
+#define INT_FAST32_WIDTH	INT32_WIDTH
+#define INT_FAST64_WIDTH	INT64_WIDTH
+#define INTPTR_WIDTH		INT64_WIDTH
+#define INTMAX_WIDTH		INT64_WIDTH
+
+/*
+ * ISO/IEC 9899:2023
+ * 7.22.3 Width of other integer types
+ */
+#define PTRDIFF_WIDTH		INT64_WIDTH
+#define SIG_ATOMIC_WIDTH	INT64_WIDTH
+#define SIZE_WIDTH		INT64_WIDTH
+#define WCHAR_WIDTH		INT32_WIDTH
+#define WINT_WIDTH		INT32_WIDTH
+#endif /* __ISO_C_VISIBLE >= 2023 */
+
 #endif /* !defined(__cplusplus) || defined(__STDC_LIMIT_MACROS) */
 
 #endif /* !_MACHINE__STDINT_H_ */