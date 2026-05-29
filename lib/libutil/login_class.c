freebsd/freebsd-src

commit 747d8ec88fc17529d3c7c06e62db56de53fb63c5

libutil: support RLIMIT_PIPEBUF
Sponsored by:	The FreeBSD Foundation
MFC after:	1 week
Differential revision:	https://reviews.freebsd.org/D46619

lib/libutil/login_class.c
@@ -65,6 +65,7 @@ static struct login_res {
     { "swapuse",         login_getcapsize, RLIMIT_SWAP    },
     { "kqueues",         login_getcapsize, RLIMIT_KQUEUES },
     { "umtxp",           login_getcapnum,  RLIMIT_UMTXP   },
+    { "pipebuf",         login_getcapnum,  RLIMIT_PIPEBUF },
     { NULL,              0,                0              }
 };
 

lib/libutil/login.conf.5
@@ -204,6 +204,7 @@ login environment.
 .It "pseudoterminals	number		Maximum number of pseudo-terminals."
 .It "swapuse	size		Maximum swap space size limit."
 .It "umtxp	number		Maximum number of process-shared pthread locks."
+.It "pipebuf"	size		Maximum size of pipe buffers."
 .El
 .Pp
 These resource limit entries actually specify both the maximum

lib/libutil/login_class.3
@@ -118,6 +118,7 @@ pseudoterminals  RLIMIT_NPTS
 swapuse          RLIMIT_SWAP
 kqueues          RLIMIT_KQUEUES
 umtxp            RLIMIT_UMTXP
+pipebuf          RLIMIT_PIPEBUF
 .Ed
 .It LOGIN_SETPRIORITY
 Set the scheduling priority for the current process based on the

b1nc0d3x/FBSD_DEV

commit aab61da870d77a37cd7bcc3237beff2bcc9b237f

login_class: Fix kqueues, pipebuf resource types
* kqueues is a count but is listed as a size

* pipebuf is a size but is listed as a count

PR:		295623
MFC after:	1 week
Fixes:          747d8ec88fc1 ("libutil: support RLIMIT_PIPEBUF")
Fixes:          85a0ddfd0b26 ("Add a resource limit for the total...")
Reviewed by:	kib
Differential Revision:	https://reviews.freebsd.org/D57333

lib/libutil/login_class.c
@@ -63,9 +63,9 @@ static struct login_res {
     { "vmemoryuse",      login_getcapsize, RLIMIT_VMEM    },
     { "pseudoterminals", login_getcapnum,  RLIMIT_NPTS    },
     { "swapuse",         login_getcapsize, RLIMIT_SWAP    },
-    { "kqueues",         login_getcapsize, RLIMIT_KQUEUES },
+    { "kqueues",         login_getcapnum,  RLIMIT_KQUEUES },
     { "umtxp",           login_getcapnum,  RLIMIT_UMTXP   },
-    { "pipebuf",         login_getcapnum,  RLIMIT_PIPEBUF },
+    { "pipebuf",         login_getcapsize, RLIMIT_PIPEBUF },
     { "vms",             login_getcapnum,  RLIMIT_VMM     },
     { NULL,              0,                0              }
 };